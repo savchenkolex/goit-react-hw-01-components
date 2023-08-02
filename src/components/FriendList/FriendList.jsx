93eb1b2a877bc748bee2aee481151a5b00d9b4bc
lex <savchenko.lex@gmail.com>
2023-08-02 19:15:17 +0300

    fix key, fix className

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,10 @@
 export default function FriendList ({friends}) {
-    console.dir(friends);
+    
     const list = friends.map((friend) => {
-        return (<li class="item">
-                <span class="status">{friend.isOnline}</span>
-                <img class="avatar" src={friend.avatar} alt="User avatar" width="48" />
-                <p class="name">{friend.name}</p>
+        return (<li key={friend.id} className="item">
+                <span className="status">{friend.isOnline}</span>
+                <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
+                <p className="name">{friend.name}</p>
                </li>
         )
     });
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,24 @@
 export default function Statistics ({title, stats}) {
     return (
-<section class="statistics">
-  {title && (<h2 class="title">{title}</h2>)}
+<section className="statistics">
+  {title && (<h2 className="title">{title}</h2>)}
 
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
+  <ul className="stat-list">
+    <li className="item">
+      <span className="label">.docx</span>
+      <span className="percentage">4%</span>
     </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
+    <li className="item">
+      <span className="label">.mp3</span>
+      <span className="percentage">14%</span>
     </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
+    <li className="item">
+      <span className="label">.pdf</span>
+      <span className="percentage">41%</span>
     </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
+    <li className="item">
+      <span className="label">.mp4</span>
+      <span className="percentage">12%</span>
     </li>
   </ul>
 </section>
