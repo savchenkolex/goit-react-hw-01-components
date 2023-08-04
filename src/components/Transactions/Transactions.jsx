f80958185dcaf4ac079cd0082c64854a11d52ade
lex <savchenko.lex@gmail.com>
2023-08-04 03:02:10 +0300

    transaction history css done

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -14,19 +14,20 @@ export default function TransactionHistory ({items}) {
     </> )
     })
     return (
-        <table className={css["transaction-history"]}>
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
-  <tbody>
-    {rows}
-  </tbody>
-</table>
-    ) 
+  <div className={css["table-section"]}>
+    <table className={css["transaction-history"]}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  </div>) 
     
 }
 
